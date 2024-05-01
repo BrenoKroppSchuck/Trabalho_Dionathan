a2c7014b24e5fd133a7279f9f2d551e6d004b2b1
BrenoKroppSchuck <brenokropp3@gmail.com>
2024-05-01 17:29:31 -0300

    Atualização para 20 - Jogadores + Estasticas

--- src/models/dao/EstatisticasDAO.js
+++ src/models/dao/EstatisticasDAO.js
@@ -2,11 +2,27 @@ const Estatisticas = require("../estatistica");
 
 let estatisticas = [
   // Altere aqui para as suas estatisticas
-  new Estatisticas({ id: 1, pontuacao: 0, jogosJogados: 0, jogosVencidos: 28, jogosPerdidos: 16 }),
-  new Estatisticas({ id: 2, pontuacao: 0, jogosJogados: 0, jogosVencidos: 23, jogosPerdidos: 29 }),
-  new Estatisticas({ id: 3, pontuacao: 0, jogosJogados: 0, jogosVencidos: 11, jogosPerdidos: 26 }),
-  new Estatisticas({ id: 4, pontuacao: 0, jogosJogados: 0, jogosVencidos: 12, jogosPerdidos: 19 }),
-  new Estatisticas({ id: 5, pontuacao: 0, jogosJogados: 0, jogosVencidos: 23, jogosPerdidos: 29 })
+  new Estatisticas({ id: 1, pontuacao: 1500, jogosJogados: 44, jogosVencidos: 28, jogosPerdidos: 16 }),
+  new Estatisticas({ id: 2, pontuacao: 1200, jogosJogados: 52, jogosVencidos: 23, jogosPerdidos: 29 }),
+  new Estatisticas({ id: 3, pontuacao: 800, jogosJogados: 37, jogosVencidos: 11, jogosPerdidos: 26 }),
+  new Estatisticas({ id: 4, pontuacao: 950, jogosJogados: 31, jogosVencidos: 12, jogosPerdidos: 19 }),
+  new Estatisticas({ id: 5, pontuacao: 1200, jogosJogados: 52, jogosVencidos: 23, jogosPerdidos: 29 }),
+  new Estatisticas({ id: 6, pontuacao: 1800, jogosJogados: 60, jogosVencidos: 45, jogosPerdidos: 15 }),
+  new Estatisticas({ id: 7, pontuacao: 1350, jogosJogados: 48, jogosVencidos: 32, jogosPerdidos: 16 }),
+  new Estatisticas({ id: 8, pontuacao: 1100, jogosJogados: 40, jogosVencidos: 20, jogosPerdidos: 20 }),
+  new Estatisticas({ id: 9, pontuacao: 1600, jogosJogados: 55, jogosVencidos: 35, jogosPerdidos: 20 }),
+  new Estatisticas({ id: 10, pontuacao: 1400, jogosJogados: 50, jogosVencidos: 28, jogosPerdidos: 22 }),
+  new Estatisticas({ id: 11, pontuacao: 1050, jogosJogados: 45, jogosVencidos: 15, jogosPerdidos: 30 }),
+  new Estatisticas({ id: 12, pontuacao: 1700, jogosJogados: 58, jogosVencidos: 42, jogosPerdidos: 16 }),
+  new Estatisticas({ id: 13, pontuacao: 1250, jogosJogados: 42, jogosVencidos: 25, jogosPerdidos: 17 }),
+  new Estatisticas({ id: 14, pontuacao: 900, jogosJogados: 38, jogosVencidos: 18, jogosPerdidos: 20 }),
+  new Estatisticas({ id: 15, pontuacao: 1450, jogosJogados: 54, jogosVencidos: 35, jogosPerdidos: 19 }),
+  new Estatisticas({ id: 16, pontuacao: 1300, jogosJogados: 47, jogosVencidos: 30, jogosPerdidos: 17 }),
+  new Estatisticas({ id: 17, pontuacao: 1550, jogosJogados: 51, jogosVencidos: 39, jogosPerdidos: 12 }),
+  new Estatisticas({ id: 18, pontuacao: 950, jogosJogados: 36, jogosVencidos: 18, jogosPerdidos: 18 }),
+  new Estatisticas({ id: 19, pontuacao: 1650, jogosJogados: 56, jogosVencidos: 38, jogosPerdidos: 18 }),
+  new Estatisticas({ id: 20, pontuacao: 1150, jogosJogados: 43, jogosVencidos: 20, jogosPerdidos: 23 })
+
 ]
 
 class EstatisticasDAO {
--- src/models/dao/JogadoresDAO.js
+++ src/models/dao/JogadoresDAO.js
@@ -3,11 +3,27 @@ const Jogador = require("../jogador")
 // Vetor de Jogadores
 let jogadores = [
   // Altere aqui para os seus jogadores
-  new Jogador({ id: 1, nome: "Dionathan L. de Vargas", nickName: "dion.vargas", classificacao: 0, estatisticas: 1, conquistas: [1, 2, 3] }),
-  new Jogador({ id: 2, nome: "Frank G. Allen Vargas", nickName: "Kuxeru", classificacao: 0, estatisticas: 2, conquistas: [5] }),
-  new Jogador({ id: 3, nome: "Lauri Astala", nickName: "Bikuir", classificacao: 0, estatisticas: 3, conquistas: [4] }),
-  new Jogador({ id: 4, nome: "Bianka Wismer", nickName: "Estuyr", classificacao: 0, estatisticas: 4, conquistas: [3, 5] }),
-  new Jogador({ id: 5, nome: "Baruch Papapanagiotou", nickName: "Viapus", classificacao: 0, estatisticas: 5, conquistas: [1, 5] })
+  new Jogador({ id: 1, nome: "Dionisio L. de Vargas", nickName: "dion_vargas96", classificacao: 0, estatisticas: 1, conquistas: [1, 2, 3] }),
+  new Jogador({ id: 2, nome: "Fernando G. Allen Vargas", nickName: "Kuxeru2", classificacao: 0, estatisticas: 2, conquistas: [5] }),
+  new Jogador({ id: 3, nome: "Lara Astala", nickName: "Bikuirer", classificacao: 0, estatisticas: 3, conquistas: [4] }),
+  new Jogador({ id: 4, nome: "Bianca Wiesmer", nickName: "EstuyrX", classificacao: 0, estatisticas: 4, conquistas: [3, 5] }),
+  new Jogador({ id: 5, nome: "Petros Papapanagiotou", nickName: "Viapuser", classificacao: 0, estatisticas: 5, conquistas: [1, 5] }),
+  new Jogador({ id: 6, nome: "Bart Papapanagiotou", nickName: "ViapusMax", classificacao: 0, estatisticas: 5, conquistas: [1, 5] }),
+  new Jogador({ id: 7, nome: "Barry Papapanagiotou", nickName: "ViapusPro", classificacao: 0, estatisticas: 5, conquistas: [1, 5] }),
+  new Jogador({ id: 8, nome: "Bruno Papapanagiotou", nickName: "ViapusGamer", classificacao: 0, estatisticas: 5, conquistas: [1, 5] }),
+  new Jogador({ id: 9, nome: "Bastian Papapanagiotou", nickName: "ViapusMaster", classificacao: 0, estatisticas: 5, conquistas: [1, 5] }),
+  new Jogador({ id: 10, nome: "Benedict Papapanagiotou", nickName: "ViapusLegend", classificacao: 0, estatisticas: 5, conquistas: [1, 5] }),
+  new Jogador({ id: 11, nome: "Bryce Papapanagiotou", nickName: "ViapusChamp", classificacao: 0, estatisticas: 5, conquistas: [1, 5] }),
+  new Jogador({ id: 12, nome: "Blaise Papapanagiotou", nickName: "ViapusElite", classificacao: 0, estatisticas: 5, conquistas: [1, 5] }),
+  new Jogador({ id: 13, nome: "Bjorn Papapanagiotou", nickName: "ViapusKing", classificacao: 0, estatisticas: 5, conquistas: [1, 5] }),
+  new Jogador({ id: 14, nome: "Bastien Papapanagiotou", nickName: "ViapusBoss", classificacao: 0, estatisticas: 5, conquistas: [1, 5] }),
+  new Jogador({ id: 15, nome: "Bobby Papapanagiotou", nickName: "ViapusWarrior", classificacao: 0, estatisticas: 5, conquistas: [1, 5] }),
+  new Jogador({ id: 16, nome: "Baxter Papapanagiotou", nickName: "ViapusNinja", classificacao: 0, estatisticas: 5, conquistas: [1, 5] }),
+  new Jogador({ id: 17, nome: "Bruce Papapanagiotou", nickName: "ViapusSniper", classificacao: 0, estatisticas: 5, conquistas: [1, 5] }),
+  new Jogador({ id: 18, nome: "Baron Papapanagiotou", nickName: "ViapusHunter", classificacao: 0, estatisticas: 5, conquistas: [1, 5] }),
+  new Jogador({ id: 19, nome: "Barnaby Papapanagiotou", nickName: "ViapusAssassin", classificacao: 0, estatisticas: 5, conquistas: [1, 5] }),
+  new Jogador({ id: 20, nome: "Boris Papapanagiotou", nickName: "ViapusWarlock", classificacao: 0, estatisticas: 5, conquistas: [1, 5] })
+
 ];
 
 class JogadoresDAO {
